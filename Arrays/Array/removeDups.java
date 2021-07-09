af572c4eebec6bb7a79e9d788e63d96cf8a4e1d9
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-07-09 13:11:25 +0100

    add removeDups

--- Arrays/Array/removeDups.java
+++ Arrays/Array/removeDups.java
@@ -2,28 +2,28 @@ package Array;
 
 import java.util.Arrays;
 
-public class removeElement {
-    private static int removesElement(int[] arr, int val) {
-        if (arr.length < 1) {
+public class removeDups {
+    private static int removeDuplicates (int[] arr) {
+        if (arr.length < 1){
             return 0;
         }
         int k = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != val) {
-                arr[k++] = arr[i];
+            if (arr[k] != arr[i]){
+                k++;
+                arr[k] = arr[i];
             }
         }
-        return k;
-    } 
+        return k + 1;
+    }
 
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4, 9, 4, 3, 2};
-        int val = 4;
+        int[] arr1 = {1, 2, 2, 2, 3, 4, 5, 5, 5};
 
         System.out.println("Length of array before removing:");
         System.out.println(arr1.length);
         System.out.println("Length of array after removing:");
-        System.out.println(removesElement(arr1, val));
+        System.out.println(removeDuplicates(arr1));
         System.out.println(Arrays.toString(arr1));
     }
     
